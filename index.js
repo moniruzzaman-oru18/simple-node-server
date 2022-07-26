e0f8ea1812819c21b4beab79904241f712b135fd
Moniruzzaman-18 <md.moniruzzaman@siarasolution.com>
2022-07-26 09:58:26 +0600

    first commit for server

--- index.js
+++ index.js
@@ -1,10 +1,83 @@
+//user id : siara-db-user; pass: sslDBuser;
 const express = require('express')
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 app.use(cors());
 app.use(express.json());
 
 const port = 8000
+
+
+
+const uri = "mongodb+srv://siara-db-user:sslDBuser@cluster0.tg9yj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    const database = client.db("Tutor-Info");
+    const usersCollection = database.collection("users");
+    //GET API
+    app.get('/users',async(req,res) =>{
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.get('/users/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const user = await usersCollection.findOne(query);
+      console.log('load an user with id: ', id);
+      res.send(user);
+    })
+
+
+    //POST API
+    app.post('/users', async(req,res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      console.log('got new user',req.body);
+      console.log('added user',result);
+      res.json(result);
+    });
+
+
+    //UPDATE API
+    app.put('/users/:id', async(req,res) =>{
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = {_id: ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: {
+            name: updatedUser.name,
+            email: updatedUser.email
+        },
+      };
+      const result = await usersCollection.updateOne(filter,updateDoc,options);
+      console.log('updating user: ', id);
+      res.json(result);
+    }) ;
+
+    //DELETE API
+    app.delete('/users/:id',async(req,res) =>{
+      const id =req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      console.log('deleting user with id: ', result);
+      res.json(result);
+    });
+
+
+  }
+  finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello & Greetings From MY-TUTE')
@@ -40,5 +113,5 @@ app.post('/users', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
